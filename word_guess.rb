f77a335dfc007dba8ca25087b0de8b4cb274a46d
Jessica <jessica.weeber@gmail.com>
2016-02-25 13:49:41 -0800

    adding comments scaffolding the program

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,13 @@
+# method that has list of words and randomly selects one when player starts game
+# prints drawing with dashes for letters matching chosen word, tracks tries 
+# take in user input - letter or word.
+# See if user input is included in out word
+# if it is correct, we print out screen a
+# if not correct, print out screen b
+
+
+
+
 class WordGuess
-  
+
 end
