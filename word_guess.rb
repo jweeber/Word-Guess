9f4f48a506b6a5e740f40d9331529394ffaa357b
Leah <leahnpetersen@gmail.com>
2016-02-25 14:01:04 -0800

    Addded select_word method to start the game

--- word_guess.rb
+++ word_guess.rb
@@ -8,6 +8,19 @@
 
 
 
-class WordGuess
-
+class NewGame
+	# method to start new game with randomly selected word
+	def initialize(word)
+		@word = word
+	end
 end
+
+
+
+# method to randomly select a word
+def select_word
+	word_array = ["mountain", "elephant", "hotdogs", "notebooks", "bicycle", "scaffolding"]
+	word = word_array.sample
+end
+select_word
+
